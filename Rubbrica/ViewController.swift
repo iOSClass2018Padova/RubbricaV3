46356b011ad3477eaa87e650ec88d1086916eb3f
Stefano Vecchiati <stefano.vecchiati@aol.com>
2018-09-24 16:22:49 +0200

    Initial Commit

---
+++ Rubbrica/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rubbrica
+//
+//  Created by stefano vecchiati on 24/09/2018.
+//  Copyright © 2018 co.eggon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
